a214ba2088e1e5af87f4d0afae022e86b1912434
Cristiano Pontes <pontescr@gmail.com>
2019-12-15 17:46:10 -0300

    Projeto criado

---
+++ src/application/Program.java
@@ -0,0 +1,47 @@
+package application;
+
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Account;
+import model.exception.DomainException;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			System.out.println("Enter account data");
+			System.out.print("Number: ");
+			int number = sc.nextInt();
+			sc.nextLine();
+			System.out.print("Holder: ");
+			String holder = sc.nextLine();
+			System.out.print("Initial Balance: ");
+			double balance = sc.nextDouble();
+			System.out.print("Withdraw limit: ");
+			double withDrawLimit = sc.nextDouble();
+			Account account = new Account(number, holder, balance, withDrawLimit);
+			System.out.println();
+			
+			System.out.print("Enter amount for withdraw: ");
+			Double amount = sc.nextDouble();
+			account.withdraw(amount);
+			
+			System.out.println(account);
+		} 
+		catch (InputMismatchException e) {
+			System.out.println("Tipos de dados inválido!");
+		}
+		catch (DomainException e) {
+			System.out.println("Withdraw error: "+ e.getMessage());
+		}
+		
+		sc.close();
+	}
+
+}
